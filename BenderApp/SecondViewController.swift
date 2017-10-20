2084ac51aedcd66e357b2788a3c2d4ea50882eb1
a.belkov <a.belkov@corp.mail.ru>
2017-10-20 16:07:09 +0300

    Initial Commit

---
+++ BenderApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BenderApp
+//
+//  Created by a.belkov on 20/10/2017.
+//  Copyright © 2017 bestK1ng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
