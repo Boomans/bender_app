3106a85c68061976f6f2d82ded7ea4c829da49f9
a.belkov <bestk1ngarthur@aol.com>
2017-10-21 22:13:35 +0300

    Add test collection

---
+++ BenderApp/Controllers/MuseumController.swift
@@ -0,0 +1,36 @@
+//
+//  FirstViewController.swift
+//  BenderApp
+//
+//  Created by bestK1ng on 20/10/2017.
+//  Copyright © 2017 bestK1ng. All rights reserved.
+//
+
+import UIKit
+
+class MuseumController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    // MARK:
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) ?? UICollectionViewCell()
+        
+        return cell
+    }
+}
